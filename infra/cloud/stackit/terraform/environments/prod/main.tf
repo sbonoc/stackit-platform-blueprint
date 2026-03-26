0df45ecd18026d1a784e24247259dbd415d9cb67
Sergi Bono <sergi.bono@gmail.com>
2026-03-26 12:44:29 +0100

    feat: add platform app and infra scaffolding

---
+++ infra/cloud/stackit/terraform/environments/prod/main.tf
@@ -0,0 +1,7 @@
+terraform {
+  required_version = ">= 1.13.0"
+}
+
+locals {
+  blueprint_environment = "stackit"
+}
---
+++ infra/cloud/stackit/terraform/modules/observability/main.tf
@@ -0,0 +1,7 @@
+terraform {
+  required_version = ">= 1.13.0"
+}
+
+locals {
+  module_contract = "blueprint-optional-module-observability"
+}
